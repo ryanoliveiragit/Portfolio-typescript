335437b3d4fc112b872c909b65da28f349a4121d
Ryan Oliveira Brito <97217108+ryanoliveiragit@users.noreply.github.com>
2022-12-22 23:45:06 -0300

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
-import {theme} from '../styles/theme'
-import '../styles/GlobalStyles.css'
+import {theme} from '../../styles/theme'
+import '../../styles/GlobalStyles.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
